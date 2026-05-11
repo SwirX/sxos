a41480a6517026fca755dca029e64bb4238153e6
SwirX <swirxytb@gmail.com>
2026-05-11 23:35:56 +0100

    chore: bump version to 2.2.0 in manifest.lua

--- manifest.lua
+++ manifest.lua
@@ -8,7 +8,7 @@ return {
 
     meta = {
         name         = "sxos-core",
-        version      = "2.1.0",
+        version      = "2.2.0",
         author       = "SwirX",
         description  = "SXOS - core operating system for ComputerCraft",
         channel      = "stable",
